1f7b36fe9ef62a260e7e7c86b2a47056922b09bc
milban <wnsgur6311@gmail.com>
2022-07-10 21:28:06 +0900

    Move apps related user-web to apps dir.

--- packages/apps/user-web/jest.config.ts
+++ packages/apps/user-web/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'user-web',
-  preset: '../../jest.preset.js',
+  displayName: 'apps-user-web',
+  preset: '../../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/packages/user-web',
+  coverageDirectory: '../../../coverage/packages/apps/user-web',
 };
