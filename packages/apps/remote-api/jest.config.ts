71a017acd86a83aaa5797d7c60b5c1413e1bb62f
milban <wnsgur6311@gmail.com>
2022-07-16 20:58:36 +0900

    Init remote api

---
+++ packages/apps/remote-api/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'apps-remote-api',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/packages/apps/remote-api',
+};
